2a0a990e0893d862b3d7738e5687e42c25b8c9f3
James Stuart Milne <jstuartmilne@Jamess-MacBook-Pro.local>
2020-05-04 15:20:23 -0300

    feat: added find adjacents

--- app/Game.scala
+++ app/Game.scala
@@ -1,13 +1,44 @@
-object Game {
+import scala.annotation.tailrec
+import scala.util.Random
 
-  def position(rowCount: Int, idx: Int): (Int, Int) = {
+object Game {
+  type Position = (Int, Int) // row, column
+
+  def position(rowCount: Int, idx: Int): Position = {
     val row = (idx % rowCount)
     val column = (idx - row) / rowCount
     (row, column)
   }
 
-  def index(rowCount: Int, position:(Int,Int)): Int = {
+  def index(rowCount: Int, position:Position): Int = {
     val (row,col) = position
     col * rowCount + row
   }
+
+  def randomBombPositionsInGrid(bombCount:Int, boardSize: Int): Set[Int] = { // Bomb position
+    @tailrec
+    def placeBombs(placedBombs:Set[Int]): Set[Int] ={
+      if (placedBombs.size == bombCount){
+        return placedBombs
+      }
+      val bombIdx = Random.nextInt(boardSize-1)
+      placeBombs(placedBombs + bombIdx)
+    }
+    placeBombs(Set())
+  }
+
+  def adjacentsForPosition(boardSize:Int, rowCount:Int, position: Position):Set[Int] = {
+    val (row, col) = position
+    val possibleCandidates = for (col <- Set(col,col+1,col-1);
+      row <-Set(row,row+1,row-1))
+      yield(row,col)
+
+    def existsInBoard: (Int) => Boolean = (idx:Int)=> (idx < boardSize && idx >=0 )
+
+    val candidatesWithoutSelf = possibleCandidates.filter(_!=position).filter((pos)=> (pos._1 >=0 && pos._2 >=0))
+    println(s"Candidates without self ${candidatesWithoutSelf}")
+    candidatesWithoutSelf.map(index(rowCount,_)).filter(existsInBoard)
+  }
+
+
 }
--- test/GameTest.scala
+++ test/GameTest.scala
@@ -15,12 +15,45 @@ class GameTest extends PlaySpec{
     result1 mustBe (1,2)
   }
 
-  "Compute the correct index given a position" in {
+  "compute the correct index given a position" in {
     val resultIdx = Game.index(3,(0,1))
     resultIdx mustBe 3
 
     val resultIdx1 = Game.index(3,(1,2))
     resultIdx1 mustBe 7
+
+    val resultidx2 = Game.index(3,(0,0))
+    resultidx2 mustBe 0
+  }
+
+  "add random bombs within grid" in {
+    val bombCount = 3
+    val boardSize = 12
+    val result = Game.randomBombPositionsInGrid(bombCount,boardSize)
+    result.size > 1 mustBe true // Works
+    result.size mustBe bombCount // Returns the appropiate size
+    result.filter(p=> p < boardSize && p >= 0 ).size mustBe bombCount //  makes sense
+  }
+  "returns adjacent positions" in {
+    val rowSize = 3
+    val colSize = 4
+    val boardSize = rowSize*colSize
+    val maxPossibleAdjacents = 8
+
+    val positionOfAMiddleNumber = (1,1)
+    val indexOfMiddleNumber = 4
+    val result = Game.adjacentsForPosition(boardSize,rowSize,positionOfAMiddleNumber) // Testing the 4 in the middle
+    result.contains(indexOfMiddleNumber) mustBe false // Should not show itself as a position
+    result.filter(rIdx=>rIdx >=0 && rIdx<boardSize).size mustBe maxPossibleAdjacents
+
+    // Gets rid of points out of the grid
+    val positionOfVertex = (0,0)
+    val indexOfVertex = 0
+    val vertexMaxAdjacents = 3
+    val result1 = Game.adjacentsForPosition(boardSize,rowSize,positionOfVertex) // Testing the 4 in the middle
+    result1.contains(indexOfVertex) mustBe false // Should not show itself as a position
+    result1.filter(rIdx=>rIdx >=0 && rIdx<boardSize).size mustBe vertexMaxAdjacents
+    result1.equals(Set(3,4,1)) mustBe true
 
   }
 }
